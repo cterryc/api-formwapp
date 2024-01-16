e8743c67e84b806ea7f4f140ebaa088762714437
cterryc <danyel.martel@gmail.com>
2024-01-16 06:47:20 -0500

    fix: db.js modificado para escuchar local y deploy

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,27 @@
 import dotenv from 'dotenv'
 import { Sequelize } from 'sequelize'
 dotenv.config()
-const { /* PORT_DB,  */USER_DB, PASS_DB/* , NAME_DB */ } = process.env
+const {
+  PORT_DB,
+  USER_DB,
+  PASS_DB,
+  NAME_DB,
+  LOCAL,
+  USER_DB_LOCAL,
+  PASS_DB_LOCAL,
+  PORT_DB_LOCAL,
+  NAME_DB_LOCAL
+} = process.env
 
-// const DATA_BASE = new Sequelize(`postgres://${USER_DB}:${PASS_DB}@${PORT_DB}/${NAME_DB}`, {
-//   logging: false
-// })
-
-const DATA_BASE = new Sequelize(`postgres://${USER_DB}:${PASS_DB}@isabelle.db.elephantsql.com/gwcpjahe`, {
-  logging: false
-})
+let DATA_BASE
+if (LOCAL) {
+  DATA_BASE = new Sequelize(`postgres://${USER_DB_LOCAL}:${PASS_DB_LOCAL}@${PORT_DB_LOCAL}/${NAME_DB_LOCAL}`, {
+    logging: false
+  })
+} else {
+  DATA_BASE = new Sequelize(`postgres://${USER_DB}:${PASS_DB}@${PORT_DB}/${NAME_DB}`, {
+    logging: false
+  })
+}
 
 export default DATA_BASE
